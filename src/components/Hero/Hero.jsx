fbb2f8f0f7b858163894deb0860675fe327db875
MHD-YOUSUF <mhdyousuf2003@gmail.com>
2024-03-07 16:00:41 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Rajesh K</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer. I build beatutiful Websites with React
+        </p>
+        <a href="https://wa.me/919677416472" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
